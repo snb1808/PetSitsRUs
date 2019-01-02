d28a7accecaeb9121af27c6bde0df6ecc3debdae
Andy <andypurbrick@gmail.com>
2019-01-02 15:32:12 +0000

    updated appointment form

--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -4,4 +4,8 @@ class Sitter < ApplicationRecord
   has_many :pets, through: :appointments
   has_many :owners, through: :appointments
 
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
 end
